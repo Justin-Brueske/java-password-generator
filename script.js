7b9c7124189694efdb290d3cfeda5cf571163e8c
Justin Brueske <justin.brueske@gmail.com>
2022-12-18 17:58:50 -0600

    intial function

--- script.js
+++ script.js
@@ -19,3 +19,48 @@ let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "0123456789";
 let special = "!@#$%^&*()";
 
+function generatePassword() {
+  let pwlength = window.prompt ("How many characters for your password (8-128)");
+  console.log(pwlength);
+  let pwlower = window.confirm ("Would you like lowercase letters");
+  console.log(pwlower);
+  let pwupper = window.confirm ("Would you like uppercase letters");
+  console.log(pwupper);
+  let pwnumbers = window.confirm ("Would you like numbers");
+  console.log(pwnumbers);
+  let pwspecial = window.confirm ("would you like special characters");
+  console.log(pwspecial);
+
+  let pwstring = '';
+  console.log(pwstring);
+  if (pwlower) {
+    pwstring = pwstring + lower;
+  }
+  console.log(pwstring);
+  if (pwupper) {
+    pwstring = pwstring + upper;
+  }
+  console.log(pwstring);  
+  if (pwnumbers) {
+    pwstring = pwstring + numbers;
+  }
+  console.log(pwstring);
+  if (pwspecial) {
+    pwstring = pwstring + special;
+  }
+  console.log(pwstring);
+  
+  let result = "";
+  let i = 0;
+  do {
+    // need to make a random string out of lower, upper, numbers, special
+    result = result + pwstring.charAt(Math.floor(Math.random() * pwstring.length));
+    console.log(result);
+    i++;
+
+  
+  
+  
+  } while (i < pwlength);
+
+}
